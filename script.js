9973e65e611fd2a8a2e6870a18137907ea791814
Ali Conteh <aliconteh24@yahoo.com>
2023-02-11 17:37:16 -0500

    Confirmed Password length

--- script.js
+++ script.js
@@ -4,6 +4,13 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  let passwordLength = prompt("What is the length of your password?");
+  if(passwordLength < 8 || passwordLength > 128 ){
+    alert("Password length must be greater than 8 and less than 128")
+    generatePassword()
+  }
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,3 +22,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
